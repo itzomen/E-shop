3009e44755cad8fd0a0e29e1e3c320f82561c9ae
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-10-06 14:40:41 +0100

    Created static and template folders, added settings and files

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'e_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'all_static')]
+
