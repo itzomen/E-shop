77f9c8e198b58db22720f3c7afe82715db94abd8
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-10-06 21:13:34 +0100

    created reduce from cart view

--- cart/views.py
+++ cart/views.py
@@ -36,6 +36,7 @@ def add_to_cart(request, slug):
         messages.info(request, f"{item.name} was added to your cart.")
         return redirect('cart:cart-summary')
 
+
 @login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
@@ -58,10 +59,47 @@ def remove_from_cart(request, slug):
             return redirect('cart:cart-summary')
         else:
             messages.info(request, f"{item.name} was not in your cart")
-            return redirect("shop:item_detail", slug=slug)
+            return render(request, 'shop/item/item.html', 
+                            {'item': item})
     else:
         messages.info(request, "You have item in your cart")
-        return redirect("shop:item_detail", slug=slug)
+        return render(request, 'shop/item/item.html', 
+                            {'item': item})
+
+
+
+@login_required
+def reduce_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    cart_qs = Cart.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if cart_qs.exists():
+        cart = cart_qs[0]
+        # check if the item is in the cart
+        if cart.items.filter(item__slug=item.slug).exists():
+            cart_item = CartItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if cart_item.quantity > 1:
+                cart_item.quantity -= 1
+                cart_item.save()
+            else:
+                cart.items.remove(order_item)
+            messages.info(request, f"{item.name} quantity was reduced from your cart.")
+            return redirect('cart:cart-summary')
+        else:
+            messages.info(request, "This item was not in your cart")
+            return render(request, 'shop/item/item.html', 
+                            {'item': item})
+    else:
+        messages.info(request, "You do not have an active order")
+        return render(request, 'shop/item/item.html', 
+                            {'item': item})
+
 
 
 class CartSummaryView(LoginRequiredMixin, View):
