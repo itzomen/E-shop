f0c4852fbd06bb0ed2b44c15af1a748478b9149b
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-10-07 01:27:00 +0100

    fixed all_items view path

--- shop/urls.py
+++ shop/urls.py
@@ -6,7 +6,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.item_list, name='item_list'),
-    path('/items/', AllView.as_view(), name='all_items'),
+    path('items/', AllView.as_view(), name='all_items'),
     path('<slug:category_slug>/', views.item_list, name='item_list_by_category'),
     path('<int:id>/<slug:slug>/', views.item_detail, name='item_detail'),
 ]
