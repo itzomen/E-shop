e4ca37a0bf9db68e7b234de9b14279dc2b1b97e2
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-10-08 15:47:13 +0100

    created context processor for cart app and added to base.html

---
+++ cart/context_processors.py
@@ -0,0 +1,6 @@
+from .models import Cart
+
+def cart(request):
+    # get user cart and add to request context
+    cart = Cart.objects.get(user=request.user, ordered=False)
+    return {'cart': cart}
