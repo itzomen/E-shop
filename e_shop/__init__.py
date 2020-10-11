5ecb37416f6761b6b4a42dcf8e1c0716da7cb894
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-10-11 04:33:31 +0100

    set Celery for E-shop

---
+++ e_shop/__init__.py
@@ -0,0 +1,3 @@
+
+# Load celery each time django starts
+from .celery import app as celery_app
---
+++ e_shop/celery.py
@@ -0,0 +1,11 @@
+import os
+from celery import Celery
+
+# Django default settings for celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_shop.settings')
+# initiating app instance
+app = Celery('e_shop')
+# Loading any custom configuration from e shop
+app.config_from_object('django.conf:settings', namespace='CELERY')
+# Setting Celery to auto discover asynchronous tasks for e shop
+app.autodiscover_tasks()
