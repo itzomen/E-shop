4d13bc088e01983ba38bcd73a454acb5d90190af
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-10-14 21:29:20 +0100

    created task.py of order app

---
+++ orders/tasks.py
@@ -0,0 +1,15 @@
+from celery import task
+from django.core.mail import send_mail
+from .models import Order
+
+@task
+def email_order(order_id):
+    """
+    This task sends the order number via email to the user
+    upon successful order
+    """
+    order = Order.objects.get(id=order_id)
+    subject = f'E-shop Order Number {order.id}'
+    message = f'Dear {order.first_name},\n\n' \
+    f'You have successfully ordered from E-shop.' \
+    f'Your order ID is {order.id}.'
