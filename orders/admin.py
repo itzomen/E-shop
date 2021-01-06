24131ad877a97916c5909af15efe7d3658fe4b86
itzomen <peng.akebuon2468@gmail.com>
2021-01-06 21:09:34 +0100

    Added export to pdf function to admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -42,13 +42,19 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
      '''Admin View for Order'''
      list_display = ['id',
                     'first_name',
                     'last_name',
-                    order_detail, 
+                    order_detail,
+                     order_pdf, 
                     'email',
                     'city',
                     'address', 
